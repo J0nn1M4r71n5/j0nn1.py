d3a916ae2f93b4dbc53004fc3e762a7ddb05e234
Jonni Silva <j0nn1@protonmail.com>
2023-10-24 19:39:03 -0700

    adicionei comentários

--- cursoemvideo-python-master/mundo-01/exercícios/ex007.py
+++ cursoemvideo-python-master/mundo-01/exercícios/ex007.py
@@ -2,3 +2,6 @@ n1 = float(input('Primeira nota do aluno: '))
 n2 = float(input('Segunda nota do aluno: '))
 média = (n1 + n2) / 2
 print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(n1, n2, média))
+
+# Use float quando tiver de esrever texto.
+# O .1f serve para colocar apenas um algarismo depois do ponto (.). Se fosse dois algarismos seria .2f.
