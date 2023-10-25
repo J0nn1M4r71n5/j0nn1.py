686595f10324c994f1ac975bc447079769af2148
Jonni Silva <j0nn1@protonmail.com>
2023-10-24 19:07:38 -0700

    pequena mudança

--- cursoemvideo-python-master/mundo-01/exercícios/ex003.py
+++ cursoemvideo-python-master/mundo-01/exercícios/ex003.py
@@ -1,4 +1,4 @@
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro valor: '))
-s = n1 + n2
-print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
+valor1 = int(input('Digite um valor: '))
+valor2 = int(input('Digite outro valor: '))
+soma = valor1 + valor2
+print('A soma entre {} e {} é igual a {}!'.format(valor1, valor2, soma))
