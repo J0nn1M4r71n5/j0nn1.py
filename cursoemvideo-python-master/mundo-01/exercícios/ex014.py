3314e3b3f5bfb5fc6ac6e169e90b769fd57ce296
Jonni Silva <j0nn1@protonmail.com>
2023-10-26 16:47:26 -0700

    C >>>F , F>>>C

--- cursoemvideo-python-master/mundo-01/exercícios/ex014.py
+++ cursoemvideo-python-master/mundo-01/exercícios/ex014.py
@@ -1,3 +1,13 @@
 c = float(input('Informe a temperatura em ˚C: '))
-f = 9 * c / 5 + 32
+f = (9 * c / 5) + 32
 print('A temperatura de {}˚C corresponde a {}˚F!'.format(c, f))
+
+F = float(input('Informe a temperatura em ˚F: '))
+C = (F - 32) * 5 / 9
+print('A temperatura de {}˚F corresponde a {}˚C!'.format(F, C))
+
+# FÓRMULA DE CONVERSÃO DE CELCIUS PARA FAHRENHEIT
+# °F = (°C x 1.8) + 32 ou °F = °C x 9 5 {\frac {9}{5}} + 32.
+
+# FÓRMULA DE CONVERSÃO DE FAHRENHEIT PARA CELCIUS
+# °C = (°F - 32) x 5 9 {\frac {5}{9}}
