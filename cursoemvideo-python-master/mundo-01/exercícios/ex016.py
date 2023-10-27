cab2f5fdb1d9e3c06979283574b8282d9b509a2c
Jonni Silva <j0nn1@protonmail.com>
2023-10-26 17:17:45 -0700

    olha só o que descobri!!!

--- cursoemvideo-python-master/mundo-01/exercícios/ex016.py
+++ cursoemvideo-python-master/mundo-01/exercícios/ex016.py
@@ -4,3 +4,7 @@ print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc
 
 num = float(input('Digite um valor: '))
 print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
+
+'''Olha só o que decobri!!!!!'''
+
+# Olha só o que descobri!!!!!
