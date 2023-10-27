2b52230640e5638724a46d553ee0d49758a8df37
Jonni Silva <j0nn1@protonmail.com>
2023-10-26 17:09:49 -0700

    km no final para ficar com 3 casas decimais

--- cursoemvideo-python-master/mundo-01/exercícios/ex015.py
+++ cursoemvideo-python-master/mundo-01/exercícios/ex015.py
@@ -1,4 +1,5 @@
 dias = int(input('Quantos dias alugados? '))
-km = float(input('Quantos Km rodados? '))
+km = float(input('Quantos Km rodados?'))
 pago = (dias * 60) + (km * 0.15)
-print('O total a pagar é de R${:.2f}'.format(pago))
+
+print('O total a pagar é de R${:.3f}'.format(pago,km))
